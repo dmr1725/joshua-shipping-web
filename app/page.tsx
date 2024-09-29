b421a9d22293ce47ee852e472a423472b087c012
Diego <diegozmendez@gmail.com>
2024-09-29 14:22:17 -0400

    Add HeroSection boilerplate.

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import {onest} from '@/app/ui/fonts';
-import Header from '@/app/ui/landing/header/header'
+import {Header} from '@/app/ui/landing/header/header'
+import { HeroSection } from './ui/landing/hero-section/hero-section';
 
 export default function Landing() {
   return (
     <div className={onest.className}>
       <Header/>
+      <HeroSection/>
     </div>
   );
 }
---
+++ app/ui/landing/hero-section/hero-section.tsx
@@ -0,0 +1,28 @@
+import {Button} from '@/app/ui/button/button'
+import Image from 'next/image';
+import styles from './hero-section.module.css'
+
+export const HeroSection = () => {
+  return (
+    <div>
+        <div className='pt-[5.75rem]'>
+            <div className={styles['hero-section-container']}>
+                <div className={styles['text-container']}>
+                    <div><strong>Streamlined</strong> logistics services for <strong>seamless</strong> deliveries</div>
+                    <div>Ensuring fresh deliveries from supplier to market, every time.</div>
+                    <Button className={`w-[7.5rem]`}>Get Quote</Button>
+                </div>
+                <div className={styles['warehouse-container']}>
+                    <Image 
+                        src='/cards/warehouse.png'
+                        alt='warehouse'
+                        layout='responsive'
+                        width={20}
+                        height={19}  
+                    />
+                </div>
+            </div>
+        </div>
+    </div>
+  );
+}
