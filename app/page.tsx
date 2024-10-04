e9a7842bec604ab55f80bb768b9012965cd22a6a
Diego <diegozmendez@gmail.com>
2024-10-04 16:22:18 -0400

    Add CurrentClients component.

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import { Header } from '@/app/ui/landing/header/header'
 import { HeroSection } from './ui/landing/hero-section/hero-section';
 import { WhatWeDo } from './ui/landing/what-we-do/what-we-do';
 import { HowItWorks } from './ui/landing/how-it-works/how-it-works';
+import { CurrentClients } from './ui/landing/current-clients/current-clients';
 
 
 export default function Landing() {
@@ -12,6 +13,7 @@ export default function Landing() {
       <HeroSection/>
       <WhatWeDo/>
       <HowItWorks/>
+      <CurrentClients/>
     </div>
   );
 }
