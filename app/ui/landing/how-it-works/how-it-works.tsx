1dda263892b9b6fe40afeb09a0f567197a95c1b4
Diego <diegozmendez@gmail.com>
2024-10-03 23:13:27 -0400

    Make step cards change in carousel when mobile view.

--- app/ui/landing/how-it-works/how-it-works.tsx
+++ app/ui/landing/how-it-works/how-it-works.tsx
@@ -1,6 +1,9 @@
+'use client';
+import { useEffect, useState } from 'react'
 import styles from './how-it-works.module.css'
 
 export const HowItWorks = () => {
+    const [currentStepId, setCurrentStepId] = useState(1);
     const steps = [
         {
             id: 1,
@@ -22,6 +25,17 @@ export const HowItWorks = () => {
         },
         
     ]
+
+    useEffect(() => {
+        // Set up an interval to change the step every 3 seconds
+        const intervalId = setInterval(() => {
+            setCurrentStepId((prevStepId) => (prevStepId === 3 ? 1 : prevStepId + 1));
+        }, 3000); // Change step every 3 seconds
+
+        // Cleanup interval on component unmount
+        return () => clearInterval(intervalId);
+    }, []);
+
     return (
         <div className={styles['how-it-works-container']}>
             <div>
@@ -30,12 +44,17 @@ export const HowItWorks = () => {
             </div>
             <div className={styles['steps-container']}>
                 {steps.map((step) => (
-                    <div key={step.id} className={styles['steps-card-container']}>
+                    <div key={step.id} className={`${styles['steps-card-container']} ${currentStepId != step.id ? styles['hide-step-card'] : ''}`}>
                         <img className={styles['step-image']} src={step.image}/>
                         <div className={styles['step-header']}>{step.header}</div>
                         <div className={styles['step-text']}>{step.text}</div>
                     </div>
                 ))}
+                <div className={styles['ellipse-container']}>
+                    <img src={`${currentStepId == 1 ? 'icons/filled_ellipse.svg' : 'icons/empty_ellipse.svg'}`}/>
+                    <img src={`${currentStepId == 2 ? 'icons/filled_ellipse.svg' : 'icons/empty_ellipse.svg'}`}/>
+                    <img src={`${currentStepId == 3 ? 'icons/filled_ellipse.svg' : 'icons/empty_ellipse.svg'}`}/>
+                </div>
             </div>
         </div>
     )
