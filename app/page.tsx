c567eea9a1d6cd343d7f5881cf6885c5bc529ec7
Diego <diegozmendez@gmail.com>
2024-10-04 22:43:27 -0400

    Make Header and Footer part of app/layout.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,18 @@
 import { onest } from '@/app/ui/fonts';
-import { Header } from '@/app/ui/landing/header/header'
 import { HeroSection } from '@/app/ui/landing/hero-section/hero-section';
 import { WhatWeDo } from '@/app/ui/landing/what-we-do/what-we-do';
 import { HowItWorks } from '@/app/ui/landing/how-it-works/how-it-works';
 import { CurrentClients } from '@/app/ui/landing/current-clients/current-clients';
 import { GetInTouch } from '@/app/ui/landing/get-in-touch/get-in-touch';
-import { Footer } from '@/app/ui/landing/footer/footer';
-
 
 export default function Landing() {
   return (
     <div className={onest.className}>
-      <Header/>
-      <HeroSection/>
-      <WhatWeDo/>
-      <HowItWorks/>
-      <CurrentClients/>
-      <GetInTouch/>
-      <Footer/>
+      <HeroSection />
+      <WhatWeDo />
+      <HowItWorks />
+      <CurrentClients />
+      <GetInTouch />
     </div>
   );
 }
