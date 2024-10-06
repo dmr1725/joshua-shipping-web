208862524f1bcf751f88a75c8e64983e99c6f7fe
Diego <diegozmendez@gmail.com>
2024-10-06 12:48:19 -0400

    Refactor main Page.

--- app/ui/landing/landing.tsx
+++ app/ui/landing/landing.tsx
@@ -1,15 +1,17 @@
-import { onest } from '@/app/ui/fonts';
 import { HeroSection } from '@/app/ui/landing/hero-section/hero-section';
 import { WhatWeDo } from '@/app/ui/landing/what-we-do/what-we-do';
 import { HowItWorks } from '@/app/ui/landing/how-it-works/how-it-works';
 import { CurrentClients } from '@/app/ui/landing/current-clients/current-clients';
 import { GetInTouch } from '@/app/ui/landing/get-in-touch/get-in-touch';
-import { Landing } from './ui/landing/landing';
 
-export default function Page() {
+export const Landing = () => {
   return (
-    <div className={onest.className}>
-      <Landing/>
-    </div>
+    <>
+      <HeroSection />
+      <WhatWeDo />
+      <HowItWorks />
+      <CurrentClients />
+      <GetInTouch />
+    </>
   );
 }
