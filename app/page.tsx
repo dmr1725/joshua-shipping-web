17299e306fe474cecdc7eb3af64ea72c6a49d236
Diego <diegozmendez@gmail.com>
2024-10-06 12:49:09 -0400

    Remove unused imports.

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,4 @@
 import { onest } from '@/app/ui/fonts';
-import { HeroSection } from '@/app/ui/landing/hero-section/hero-section';
-import { WhatWeDo } from '@/app/ui/landing/what-we-do/what-we-do';
-import { HowItWorks } from '@/app/ui/landing/how-it-works/how-it-works';
-import { CurrentClients } from '@/app/ui/landing/current-clients/current-clients';
-import { GetInTouch } from '@/app/ui/landing/get-in-touch/get-in-touch';
 import { Landing } from './ui/landing/landing';
 
 export default function Page() {
