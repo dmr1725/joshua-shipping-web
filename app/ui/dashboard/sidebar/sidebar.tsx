5c9abe58dbc441e070f442c49a2536703c4896dd
Diego <diegozmendez@gmail.com>
2024-10-11 14:14:47 -0400

    Finish rough draft for Sidebar component.

--- app/ui/dashboard/sidebar/sidebar.tsx
+++ app/ui/dashboard/sidebar/sidebar.tsx
@@ -1,15 +1,36 @@
+import { Logo } from '../../logo';
+import styles from './sidebar.module.css';
 
 export const SideBar = () => {
     return (
-        <div>
-            <div>Logo</div>
-            <div>
-                <div>Profile</div>
-                <div>Orders</div>
-                <div>Payments</div>
-                <div>Dispatch Requests</div>
+        <div className={styles['main-container']}>
+            <div className={styles['sidebar-info']}>
+                <div className={styles['logo']}>
+                    <Logo src='/logo/light_jsl.svg' />
+                </div>
+                <div className={styles['sidebar-options']}>
+                    <div className={styles['icon-info']}>
+                        <img className={styles['icon']} src="icons/dashboard.svg" alt="container"/>
+                        <div>Profile</div>
+                    </div>
+                    <div className={styles['icon-info']}>
+                        <img className={styles['icon']} src="icons/container2.svg" alt="container"/>
+                        <div>Orders</div>
+                    </div>
+                    <div className={styles['icon-info']}>
+                        <img className={styles['icon']} src="icons/payments.svg" alt="container"/>
+                        <div>Payments</div>
+                    </div>
+                    <div className={styles['icon-info']}>
+                        <img className={styles['icon']} src="icons/truck2.svg" alt="container"/>
+                        <div>Dispatches</div>
+                    </div>
+                </div>
             </div>
-            <div>Log out</div>
+            <div className={styles['icon-info']}>
+                <img className={styles['icon']} src="icons/logout.svg" alt="container"/>
+                <div>Log Out</div>
+            </div>
         </div>
     )
 }
