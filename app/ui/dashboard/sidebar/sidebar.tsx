8a8e80cf0e9666fa0f3c7a0b449acb76a08edfe8
Diego <diegozmendez@gmail.com>
2024-10-11 12:58:55 -0400

    Add dashboard layout boilerplate.

---
+++ app/ui/dashboard/sidebar/sidebar.tsx
@@ -0,0 +1,15 @@
+
+export const SideBar = () => {
+    return (
+        <div>
+            <div>Logo</div>
+            <div>
+                <div>Profile</div>
+                <div>Orders</div>
+                <div>Payments</div>
+                <div>Dispatch Requests</div>
+            </div>
+            <div>Log out</div>
+        </div>
+    )
+}
