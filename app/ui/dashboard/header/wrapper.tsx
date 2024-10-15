a60a5d191c7318cb2f1ee637738b560994148839
Diego <diegozmendez@gmail.com>
2024-10-15 17:07:39 -0400

    Add wrapper for DashboardHeader.

---
+++ app/ui/dashboard/header/wrapper.tsx
@@ -0,0 +1,12 @@
+import { fetchCompanyName } from "@/app/lib/data";
+import { DashboardHeader } from "./header";
+
+export default async function DashboardHeaderWrapper() {
+    const companyName = await fetchCompanyName();
+
+    return (
+        <>
+            <DashboardHeader companyName={companyName}/>
+        </>
+    )
+}
