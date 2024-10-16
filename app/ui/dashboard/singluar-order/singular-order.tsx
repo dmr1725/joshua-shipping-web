5b1de98bbcea77e3ae14252b3e5337c7d34633d2
Diego <diegozmendez@gmail.com>
2024-10-16 18:39:42 -0400

    Add SingularOrder component.

---
+++ app/ui/dashboard/singluar-order/singular-order.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import styles from './singular-order.module.css'
+import { OrderInterface } from '@/app/lib/data'
+
+export const SingluarOrder: React.FC<OrderInterface> = ({id, bl, container_no, date, lots}) => {
+    return (
+        <div className={styles['order-container']}>
+            <div className={styles['order-details']}>
+                <div>
+                    <div>Order placed</div>
+                    <div>{date}</div>
+                </div>
+                <div>
+                    <div>Bill of Lading</div>
+                    <div>{bl}</div>
+                </div>
+                <div>
+                    <div>Container #</div>
+                    <div>{container_no}</div>
+                </div>
+                <div>
+                    <div>Order #{id}</div>
+                    <div>View order details</div>
+                </div>
+            </div>
+            
+            <div className={styles['lots-container']}>
+                {lots.map((lot) => (
+                    <div key={lot.id} className={styles['lot-container']}>
+                        <div className={styles['lot-details']}>
+                            <div className={styles['lot-font']}>Lot #{lot.id}</div>
+                            <div className={styles['lot-product']}>
+                                <div>{lot.product}: {lot.current_boxes}/{lot.starting_boxes}</div>
+                                <img className={styles['box-icon']} src="icons/box.svg" alt="box" />
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
+}
---
+++ app/ui/dashboard/singluar-order/skeleton.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import styles from './singular-order.module.css';
+
+export const SingularOrderSkeleton = () => {
+    const shimmer = `before:absolute before:inset-0 before:-translate-x-full before:animate-[shimmer_2s_infinite] 
+    before:bg-gradient-to-r before:from-transparent before:via-white/60 before:to-transparent relative overflow-hidden bg-gray-200`;
+
+    return (
+        <div className={`${styles['order-container']} ${shimmer}`}>
+            <div className={`${styles['order-details']}`}>
+                <div>
+                    <div className={`${shimmer}`} style={{ width: '100px', height: '20px', marginBottom: '10px' }}></div>
+                    <div className={`${shimmer}`} style={{ width: '150px', height: '20px' }}></div>
+                </div>
+                <div>
+                    <div className={`${shimmer}`} style={{ width: '120px', height: '20px', marginBottom: '10px' }}></div>
+                    <div className={`${shimmer}`} style={{ width: '180px', height: '20px' }}></div>
+                </div>
+                <div>
+                    <div className={`${shimmer}`} style={{ width: '120px', height: '20px', marginBottom: '10px' }}></div>
+                    <div className={`${shimmer}`} style={{ width: '180px', height: '20px' }}></div>
+                </div>
+                <div>
+                    <div className={`${shimmer}`} style={{ width: '120px', height: '20px', marginBottom: '10px' }}></div>
+                    <div className={`${shimmer}`} style={{ width: '80px', height: '20px' }}></div>
+                </div>
+            </div>
+
+            <div className={styles['lots-container']}>
+                <div className={`${styles['lot-container']} ${shimmer}`}>
+                    <div className={styles['lot-details']}>
+                        <div className={`${shimmer}`} style={{ width: '60px', height: '20px', marginBottom: '10px' }}></div>
+                        <div className={styles['lot-product']}>
+                            <div className={`${shimmer}`} style={{ width: '200px', height: '20px' }}></div>
+                            <div className={`${shimmer}`} style={{ width: '30px', height: '30px' }}></div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className={`${styles['lot-container']} ${shimmer}`}>
+                    <div className={styles['lot-details']}>
+                        <div className={`${shimmer}`} style={{ width: '60px', height: '20px', marginBottom: '10px' }}></div>
+                        <div className={styles['lot-product']}>
+                            <div className={`${shimmer}`} style={{ width: '200px', height: '20px' }}></div>
+                            <div className={`${shimmer}`} style={{ width: '30px', height: '30px' }}></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
