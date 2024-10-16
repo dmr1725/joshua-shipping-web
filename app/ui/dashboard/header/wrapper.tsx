4726bcc29a5d924d70f69697254be7e39763cf62
Diego <diegozmendez@gmail.com>
2024-10-15 22:59:02 -0400

    Improve .content-layout responsiveness in mobile and desktop.

--- app/ui/dashboard/header/wrapper.tsx
+++ app/ui/dashboard/header/wrapper.tsx
@@ -3,7 +3,6 @@ import { DashboardHeader } from "./header";
 
 export default async function DashboardHeaderWrapper() {
     const companyName = await fetchCompanyName();
-    console.log("dimelo")
 
     return (
         <>
