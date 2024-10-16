f4724a98502863694304754cca6ef741f2bbd9b0
Diego <diegozmendez@gmail.com>
2024-10-16 16:15:56 -0400

    Save mock orders.

--- app/ui/dashboard/orders/orders.tsx
+++ app/ui/dashboard/orders/orders.tsx
@@ -1,7 +1,9 @@
+import React from 'react';
 import styles from './orders.module.css'
 import { Button } from '../../button/button'
+import { OrderProps } from '@/app/lib/data';
 
-export const Orders = () => {
+export const Orders: React.FC<OrderProps[]> = ({orders}) => {
     return (
         <div className={styles['orders-layout']}>
             <div className={styles['orders-header-font']}>
@@ -17,6 +19,334 @@ export const Orders = () => {
                 </div>
             </div>
             <div className={styles['orders-container']}>
+                <div className={styles['order-container']}>
+                    <div className={styles['order-details']}>
+                        <div>
+                            <div>Order placed</div>
+                            <div>October 16, 2024</div>
+                        </div>
+                        <div>
+                            <div>Bill of Lading</div>
+                            <div>CWPS4M097234</div>
+                        </div>
+                        <div>
+                            <div>Container #</div>
+                            <div>CMCU 5560770</div>
+                        </div>
+                        <div>
+                            <div>Order # 114-4893-859</div>
+                            <div>View order details</div>
+                        </div>
+                    </div>
+                    
+                    <div className={styles['lots-container']}>
+                        <div className={styles['lot-container']}>
+                            <div className={styles['lot-details']}>
+                                <div className={styles['lot-font']}>Lot #1829</div>
+                                <div className={styles['lot-product']}>
+                                    <div>Frozen Pork Meats: 800/800</div>
+                                    <img className={styles['box-icon']} src="icons/box.svg" alt="box" />
+                                </div>
+                            </div>
+                        </div>
+                        <div className={styles['lot-container']}>
+                            <div className={styles['lot-details']}>
+                                <div className={styles['lot-font']}>Lot #1830</div>
+                                <div className={styles['lot-product']}>
+                                    <div>Sweet Potato: 800/800</div>
+                                    <img className={styles['box-icon']} src="icons/box.svg" alt="box" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles['order-container']}>
+                    <div className={styles['order-details']}>
+                        <div>
+                            <div>Order placed</div>
+                            <div>October 16, 2024</div>
+                        </div>
+                        <div>
+                            <div>Bill of Lading</div>
+                            <div>CWPS4M097234</div>
+                        </div>
+                        <div>
+                            <div>Container #</div>
+                            <div>CMCU 5560770</div>
+                        </div>
+                        <div>
+                            <div>Order # 114-4893-859</div>
+                            <div>View order details</div>
+                        </div>
+                    </div>
+                    
+                    <div className={styles['lots-container']}>
+                        <div className={styles['lot-container']}>
+                            <div className={styles['lot-details']}>
+                                <div className={styles['lot-font']}>Lot #1829</div>
+                                <div className={styles['lot-product']}>
+                                    <div>Frozen Pork Meats: 800/800</div>
+                                    <img className={styles['box-icon']} src="icons/box.svg" alt="box" />
+                                </div>
+                            </div>
+                        </div>
+                        <div className={styles['lot-container']}>
+                            <div className={styles['lot-details']}>
+                                <div>Lot #1830</div>
+                                <div className={styles['lot-product']}>
+                                    <div>Sweet Potato: 800/800</div>
+                                    <img className={styles['box-icon']} src="icons/box.svg" alt="box" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles['order-container']}>
+                    <div className={styles['order-details']}>
+                        <div>
+                            <div>Order placed</div>
+                            <div>October 16, 2024</div>
+                        </div>
+                        <div>
+                            <div>Bill of Lading</div>
+                            <div>CWPS4M097234</div>
+                        </div>
+                        <div>
+                            <div>Container #</div>
+                            <div>CMCU 5560770</div>
+                        </div>
+                        <div>
+                            <div>Order # 114-4893-859</div>
+                            <div>View order details</div>
+                        </div>
+                    </div>
+                    
+                    <div className={styles['lots-container']}>
+                        <div className={styles['lot-container']}>
+                            <div className={styles['lot-details']}>
+                                <div className={styles['lot-font']}>Lot #1829</div>
+                                <div className={styles['lot-product']}>
+                                    <div>Frozen Pork Meats: 800/800</div>
+                                    <img className={styles['box-icon']} src="icons/box.svg" alt="box" />
+                                </div>
+                            </div>
+                        </div>
+                        <div className={styles['lot-container']}>
+                            <div className={styles['lot-details']}>
+                                <div>Lot #1830</div>
+                                <div className={styles['lot-product']}>
+                                    <div>Sweet Potato: 800/800</div>
+                                    <img className={styles['box-icon']} src="icons/box.svg" alt="box" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles['order-container']}>
+                    <div className={styles['order-details']}>
+                        <div>
+                            <div>Order placed</div>
+                            <div>October 16, 2024</div>
+                        </div>
+                        <div>
+                            <div>Bill of Lading</div>
+                            <div>CWPS4M097234</div>
+                        </div>
+                        <div>
+                            <div>Container #</div>
+                            <div>CMCU 5560770</div>
+                        </div>
+                        <div>
+                            <div>Order # 114-4893-859</div>
+                            <div>View order details</div>
+                        </div>
+                    </div>
+                    
+                    <div className={styles['lots-container']}>
+                        <div className={styles['lot-container']}>
+                            <div className={styles['lot-details']}>
+                                <div className={styles['lot-font']}>Lot #1829</div>
+                                <div className={styles['lot-product']}>
+                                    <div>Frozen Pork Meats: 800/800</div>
+                                    <img className={styles['box-icon']} src="icons/box.svg" alt="box" />
+                                </div>
+                            </div>
+                        </div>
+                        <div className={styles['lot-container']}>
+                            <div className={styles['lot-details']}>
+                                <div>Lot #1830</div>
+                                <div className={styles['lot-product']}>
+                                    <div>Sweet Potato: 800/800</div>
+                                    <img className={styles['box-icon']} src="icons/box.svg" alt="box" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles['order-container']}>
+                    <div className={styles['order-details']}>
+                        <div>
+                            <div>Order placed</div>
+                            <div>October 16, 2024</div>
+                        </div>
+                        <div>
+                            <div>Bill of Lading</div>
+                            <div>CWPS4M097234</div>
+                        </div>
+                        <div>
+                            <div>Container #</div>
+                            <div>CMCU 5560770</div>
+                        </div>
+                        <div>
+                            <div>Order # 114-4893-859</div>
+                            <div>View order details</div>
+                        </div>
+                    </div>
+                    
+                    <div className={styles['lots-container']}>
+                        <div className={styles['lot-container']}>
+                            <div className={styles['lot-details']}>
+                                <div className={styles['lot-font']}>Lot #1829</div>
+                                <div className={styles['lot-product']}>
+                                    <div>Frozen Pork Meats: 800/800</div>
+                                    <img className={styles['box-icon']} src="icons/box.svg" alt="box" />
+                                </div>
+                            </div>
+                        </div>
+                        <div className={styles['lot-container']}>
+                            <div className={styles['lot-details']}>
+                                <div>Lot #1830</div>
+                                <div className={styles['lot-product']}>
+                                    <div>Sweet Potato: 800/800</div>
+                                    <img className={styles['box-icon']} src="icons/box.svg" alt="box" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles['order-container']}>
+                    <div className={styles['order-details']}>
+                        <div>
+                            <div>Order placed</div>
+                            <div>October 16, 2024</div>
+                        </div>
+                        <div>
+                            <div>Bill of Lading</div>
+                            <div>CWPS4M097234</div>
+                        </div>
+                        <div>
+                            <div>Container #</div>
+                            <div>CMCU 5560770</div>
+                        </div>
+                        <div>
+                            <div>Order # 114-4893-859</div>
+                            <div>View order details</div>
+                        </div>
+                    </div>
+                    
+                    <div className={styles['lots-container']}>
+                        <div className={styles['lot-container']}>
+                            <div className={styles['lot-details']}>
+                                <div className={styles['lot-font']}>Lot #1829</div>
+                                <div className={styles['lot-product']}>
+                                    <div>Frozen Pork Meats: 800/800</div>
+                                    <img className={styles['box-icon']} src="icons/box.svg" alt="box" />
+                                </div>
+                            </div>
+                        </div>
+                        <div className={styles['lot-container']}>
+                            <div className={styles['lot-details']}>
+                                <div>Lot #1830</div>
+                                <div className={styles['lot-product']}>
+                                    <div>Sweet Potato: 800/800</div>
+                                    <img className={styles['box-icon']} src="icons/box.svg" alt="box" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles['order-container']}>
+                    <div className={styles['order-details']}>
+                        <div>
+                            <div>Order placed</div>
+                            <div>October 16, 2024</div>
+                        </div>
+                        <div>
+                            <div>Bill of Lading</div>
+                            <div>CWPS4M097234</div>
+                        </div>
+                        <div>
+                            <div>Container #</div>
+                            <div>CMCU 5560770</div>
+                        </div>
+                        <div>
+                            <div>Order # 114-4893-859</div>
+                            <div>View order details</div>
+                        </div>
+                    </div>
+                    
+                    <div className={styles['lots-container']}>
+                        <div className={styles['lot-container']}>
+                            <div className={styles['lot-details']}>
+                                <div className={styles['lot-font']}>Lot #1829</div>
+                                <div className={styles['lot-product']}>
+                                    <div>Frozen Pork Meats: 800/800</div>
+                                    <img className={styles['box-icon']} src="icons/box.svg" alt="box" />
+                                </div>
+                            </div>
+                        </div>
+                        <div className={styles['lot-container']}>
+                            <div className={styles['lot-details']}>
+                                <div>Lot #1830</div>
+                                <div className={styles['lot-product']}>
+                                    <div>Sweet Potato: 800/800</div>
+                                    <img className={styles['box-icon']} src="icons/box.svg" alt="box" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles['order-container']}>
+                    <div className={styles['order-details']}>
+                        <div>
+                            <div>Order placed</div>
+                            <div>October 16, 2024</div>
+                        </div>
+                        <div>
+                            <div>Bill of Lading</div>
+                            <div>CWPS4M097234</div>
+                        </div>
+                        <div>
+                            <div>Container #</div>
+                            <div>CMCU 5560770</div>
+                        </div>
+                        <div>
+                            <div>Order # 114-4893-859</div>
+                            <div>View order details</div>
+                        </div>
+                    </div>
+                    
+                    <div className={styles['lots-container']}>
+                        <div className={styles['lot-container']}>
+                            <div className={styles['lot-details']}>
+                                <div className={styles['lot-font']}>Lot #1829</div>
+                                <div className={styles['lot-product']}>
+                                    <div>Frozen Pork Meats: 800/800</div>
+                                    <img className={styles['box-icon']} src="icons/box.svg" alt="box" />
+                                </div>
+                            </div>
+                        </div>
+                        <div className={styles['lot-container']}>
+                            <div className={styles['lot-details']}>
+                                <div>Lot #1830</div>
+                                <div className={styles['lot-product']}>
+                                    <div>Sweet Potato: 800/800</div>
+                                    <img className={styles['box-icon']} src="icons/box.svg" alt="box" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
                 <div className={styles['order-container']}>
                     <div className={styles['order-details']}>
                         <div>
