e764d758091d3965d7cdc9aa5460123c0d125121
Diego <diegozmendez@gmail.com>
2024-10-18 13:32:03 -0400

    Add skeleton for Lots container.

--- app/ui/dashboard/orders/order-item/skeleton.tsx
+++ app/ui/dashboard/orders/order-item/skeleton.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import styles from './order-item.module.css';
+import { shimmer } from '@/app/ui/shimmer';
+import { LotsSkeleton } from '../../lots/lots-skeleton';
 
 export const OrderItemSkeleton = () => {
-    const shimmer = `before:absolute before:inset-0 before:-translate-x-full before:animate-[shimmer_2s_infinite] 
-    before:bg-gradient-to-r before:from-transparent before:via-white/60 before:to-transparent relative overflow-hidden bg-gray-200`;
-
+    
     return (
         <div className={`${styles['order-container']} ${shimmer}`}>
             <div className={`${styles['order-details']}`}>
@@ -26,31 +26,7 @@ export const OrderItemSkeleton = () => {
                 </div>
             </div>
 
-            {/* lots container */}
-            <div>
-                {/* lot container */}
-                <div className={`${shimmer}`}>
-                    {/* lot details */}
-                    <div>
-                        <div className={`${shimmer}`} style={{ width: '60px', height: '20px', marginBottom: '10px' }}></div>
-                        {/* lot product */}
-                        <div>
-                            <div className={`${shimmer}`} style={{ width: '200px', height: '20px' }}></div>
-                            <div className={`${shimmer}`} style={{ width: '30px', height: '30px' }}></div>
-                        </div>
-                    </div>
-                </div>
-
-                <div className={`${shimmer}`}>
-                    <div>
-                        <div className={`${shimmer}`} style={{ width: '60px', height: '20px', marginBottom: '10px' }}></div>
-                        <div>
-                            <div className={`${shimmer}`} style={{ width: '200px', height: '20px' }}></div>
-                            <div className={`${shimmer}`} style={{ width: '30px', height: '30px' }}></div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <LotsSkeleton/>
         </div>
     );
 };
