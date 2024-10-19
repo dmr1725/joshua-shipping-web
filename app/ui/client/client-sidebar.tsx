1a3f3d6a34832621e1d6752131b97f85e0e473da
Diego <diegozmendez@gmail.com>
2024-10-19 09:00:23 -0400

    Reset order.

--- app/ui/client/client-sidebar.tsx
+++ app/ui/client/client-sidebar.tsx
@@ -2,8 +2,8 @@ import { SideBar } from "@/app/ui/common/sidebar/sidebar";
 
 const clientOptions = [
     { label: 'Containers', icon: '/icons/container2.svg', link: '/dashboard/containers' },
-    { label: 'Invoices', icon: '/icons/invoice.svg', link: '/dashboard/invoices' },
-    { label: 'Dispatches', icon: '/icons/truck2.svg', link: '/dashboard/dispatches' }
+    { label: 'Dispatches', icon: '/icons/truck2.svg', link: '/dashboard/dispatches' },
+    { label: 'Invoices', icon: '/icons/invoice.svg', link: '/dashboard/invoices' }
 ];
 
 export default function ClientSidebar() {
