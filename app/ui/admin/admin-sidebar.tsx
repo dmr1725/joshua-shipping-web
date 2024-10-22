c0d60b9c9832de3fc4a86c1de63c787f84c7d4a2
David Mendez <davidmendezrosa@gmail.com>
2024-10-21 22:16:51 -0400

    fixing icon imports in admin-sidebar component

--- app/ui/admin/admin-sidebar.tsx
+++ app/ui/admin/admin-sidebar.tsx
@@ -1,9 +1,9 @@
 import { SideBar } from "@/app/ui/common/sidebar/sidebar";
 
 const AdminOptions = [
-    {label: 'Containers', icon: 'icons/container2.svg', link: '/admin-dashboard/containers'},
-    {label: 'Dispatches', icon: 'icons/truck2.svg', link: '/admin-dashboard/dispatches'},
-    {label: 'Invoices', icon: 'icons/invoice.svg', link: '/admin-dashboard/invoices'}
+    {label: 'Containers', icon: '/icons/container2.svg', link: '/admin-dashboard/containers'},
+    {label: 'Dispatches', icon: '/icons/truck2.svg', link: '/admin-dashboard/dispatches'},
+    {label: 'Invoices', icon: '/icons/invoice.svg', link: '/admin-dashboard/invoices'}
 ]
 
 export default function AdminSideBar(){
