102a4ec6f6c1a4736562dc327f20365633a65d17
Diego <diegozmendez@gmail.com>
2024-10-22 10:29:42 -0400

    Fix naming in present tense.

--- app/ui/client/dispatch-orders.tsx
+++ app/ui/client/dispatch-orders.tsx
@@ -1,15 +1,15 @@
-import { fetchAllDispatchedOrders } from "@/app/lib/data";
-import { ClientOrders } from './client-orders'
+import { fetchAllDispatchOrders } from "@/app/lib/data";
+import { ClientOrders } from "./client-orders";
 
 export default async function DispatchOrders() {
-    const orders = await fetchAllDispatchedOrders();
+  const orders = await fetchAllDispatchOrders();
 
-    return (
-        <ClientOrders 
-            orders={orders} 
-            orderType="dispatches"
-            statusOptions={["Pending", "Picked Up"]}
-            dateOptions={["1 week", "3 months", "2024"]}
-        />
-    );
+  return (
+    <ClientOrders
+      orders={orders}
+      orderType="dispatches"
+      statusOptions={["Pending", "Picked Up"]}
+      dateOptions={["1 week", "3 months", "2024"]}
+    />
+  );
 }
