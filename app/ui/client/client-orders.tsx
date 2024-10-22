18797a5f48ad3cc5180993ff9a1ff525434edef5
Diego <diegozmendez@gmail.com>
2024-10-22 10:06:21 -0400

    Refactor order filter.

--- app/ui/client/client-orders.tsx
+++ app/ui/client/client-orders.tsx
@@ -8,10 +8,7 @@ import {
   ContainerReceiptInterface,
   DispatchOrderInterface,
 } from "@/app/lib/data";
-import {
-  filterContainersReceipt,
-  filterDispatchOrders,
-} from "@/app/ui/common/orders/utils/search-filter";
+import { getFilteredOrders } from "@/app/ui/common/orders/utils/order-filters"; // Import the new function
 import {
   renderContainerReceiptDetails,
   renderDispatchOrderDetails,
@@ -51,63 +48,13 @@ export const ClientOrders: React.FC<OrdersProps> = ({
     setSelectedDate(date); // Set the selected date
   };
 
-  const filteredOrders =
-  orderType === "containers"
-    ? filterContainersReceipt(orders, searchTerm).filter((order) => {
-        const normalizedStatus =
-          order.status === "Confirmed Inventory"
-            ? order.status
-            : "Pending";
-
-        // Filter by status
-        const statusMatch = selectedStatus ? normalizedStatus === selectedStatus : true;
-
-        // Filter by date range
-        const dateMatch = filterByDate(order.date, selectedDate);
-
-        return statusMatch && dateMatch;
-      })
-    : filterDispatchOrders(orders, searchTerm).filter((order) => {
-        const normalizedStatus = order.status === "Picked Up" ? order.status : "Pending";
-
-        // Filter by status
-        const statusMatch = selectedStatus ? normalizedStatus === selectedStatus : true;
-
-        // Filter by date range
-        const dateMatch = filterByDate(order.date, selectedDate);
-
-        return statusMatch && dateMatch;
-      });
-
-    // Helper function to filter by date range
-    function filterByDate(orderDate: string | Date, selectedDate: string): boolean {
-      const currentDate = new Date();
-      const orderDateObj = new Date(orderDate);
-
-      switch (selectedDate) {
-        case "1 week": {
-          const oneWeekAgo = new Date();
-          oneWeekAgo.setDate(currentDate.getDate() - 7);
-          return orderDateObj >= oneWeekAgo && orderDateObj <= currentDate;
-        }
-        case "3 months": {
-          const threeMonthsAgo = new Date();
-          threeMonthsAgo.setMonth(currentDate.getMonth() - 3);
-          return orderDateObj >= threeMonthsAgo && orderDateObj <= currentDate;
-        }
-        case "year": {
-          const startOfYear = new Date(currentDate.getFullYear(), 0, 1); // January 1st of the current year
-          return orderDateObj >= startOfYear && orderDateObj <= currentDate;
-        }
-        case "2024": {
-          const startOf2024 = new Date(2024, 0, 1); // January 1, 2024
-          const endOf2024 = new Date(2024, 11, 31); // December 31, 2024
-          return orderDateObj >= startOf2024 && orderDateObj <= endOf2024;
-        }
-        default:
-          return true; // If no date filter is selected, return all orders
-      }
-    }
+  const filteredOrders = getFilteredOrders(
+    orders,
+    orderType,
+    searchTerm,
+    selectedStatus,
+    selectedDate
+  );
 
   const renderDetails =
     orderType === "containers"
