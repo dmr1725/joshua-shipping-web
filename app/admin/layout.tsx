db1af68a6b2679613329e45e772216a8ed6e0884
David Mendez <davidmendezrosa@gmail.com>
2024-10-22 12:33:37 -0400

    change admin-dashboard route to admin

---
+++ app/admin/layout.tsx
@@ -0,0 +1,23 @@
+import AdminSideBar from "../ui/admin/admin-sidebar";
+import DashboardHeaderWrapper from "../ui/common/header/admin-wrapper";
+import { DashboardHeaderSkeleton } from "../ui/common/header/skeleton";
+import { Suspense } from "react";
+import styles from "./layout.module.css";
+
+export default function Layout({ children } : { children: React.ReactNode}){
+    return (
+        <div className={styles["dashboard-layout"]}>
+      <div className={styles["sidebar-layout"]}>
+        <AdminSideBar />
+      </div>
+      <div className={styles["content-layout"]}>
+        <div className={styles["header-layout"]}>
+          <Suspense fallback={<DashboardHeaderSkeleton />}>
+            <DashboardHeaderWrapper />
+          </Suspense>
+        </div>
+        <div className={styles["children-layout"]}>{children}</div>
+      </div>
+    </div>
+    );
+}
