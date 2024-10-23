9ddc6a69f0e7dceed1b6931da35e31429b39cdbe
David Mendez <davidmendezrosa@gmail.com>
2024-10-23 17:16:37 -0400

    old versions of header files

--- app/ui/common/header/admin-wrapper.tsx
+++
@@ -1,9 +0,0 @@
-import { DashboardHeader } from "./header";
-
-export default function AdminDashboardHeader() {
-    return (
-        <>
-            <DashboardHeader companyName="Joshua Shipping" />
-        </>
-    );
-}
