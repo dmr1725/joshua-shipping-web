33b9e1d5433af26b320d4c620a53c1553e1bd555
Diego <diegozmendez@gmail.com>
2024-10-24 14:33:12 -0400

    Add Page to view receipt details.

--- app/dashboard/containers/[id]/page.tsx
+++ app/dashboard/containers/[id]/page.tsx
@@ -1,10 +1,10 @@
-import {ProgressTracker} from "@/app/ui/common/progress-tracker/progress-tracker";
+import { fetchReceiptById } from "@/app/lib/data";
+import { ReceiptDetails } from "@/app/ui/client/receipt-details/receipt-details";
  
 export default async function Page({ params }: { params: { id: string } }) {
     const id = params.id;
+    const receipt = await fetchReceiptById(id)
     return (
-        <>
-            <ProgressTracker/>
-        </>
+        <ReceiptDetails receipt={receipt}/>
     );
 }
